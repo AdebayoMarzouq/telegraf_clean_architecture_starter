3c829b9ef1fbc42d23b8c22fad369f676f538377
AdebayoMarzouq <marzouqdevmail@gmail.com>
2024-05-20 11:39:24 +0100

    Added code format tool for the boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier'
+    'prettier',
   ],
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'prettier/prettier': 'error'
-  }
+    'prettier/prettier': 'error',
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,9 @@ module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@core/(.*)$': '<rootDir>/src/core/$1',
-    '^@infra/(.*)$': '<rootDir>/src/infra/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@domain/(.*)$': '<rootDir>/src/domain/$1',
-    '^@presentation/(.*)$': '<rootDir>/src/presentation/$1'
-  }
+    '^@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+  },
 };
